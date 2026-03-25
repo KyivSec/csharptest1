aeb7b2031572a5e055f6fdc761df64272da3e429
Admin <Admin@DESKTOP-A04AQ48>
2026-03-25 11:51:59 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+public class TextHandler {
+    public delegate string TextOperation(string Text);
+
+    public void ProcessFile(string Path) {
+        if (File.Exists(Path)) {
+
+        } else {
+            Console.WriteLine("File does not exist.");
+        }
+    }
+
+}
